070518f381eba00501896ec248845383046a3010
MAKS <MAKS@DESKTOP-P40RUSV>
2023-12-13 21:18:45 +0500

    Добавьте файлы проекта.

---
+++ Blazor1_(13.12.23)/TaskItem.cs
@@ -0,0 +1,10 @@
+﻿namespace Blazor1__13._12._23_
+{
+    public class TaskItem
+    {
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime CreateDate { get; set; }
+    }
+}
